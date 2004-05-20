b39377e62c36f8e35a252f0b0a4fc784e35c8d91
rhettg <rhettg>
2004-05-20 20:06:52 +0000

    .

--- LogMonitor/find_reader_job.py
+++ LogMonitor/find_reader_job.py
@@ -20,41 +20,106 @@ class CheckForReaderTimer(timer.Timer):
         timer.Timer.__init__(self, FIND_READER_TIMEOUT, evt)
 
 class FindReaderJob(job.Job):
-    """The FindLogJob continuously polls a directory for new log files.
+    """The FindReaderJob finds LogReader agents to read files that have
+    been updated.
        """
        
     def __init__(self, agent_obj):
         job.Job.__init__(self, agent_obj)
 
         self._needs_reading = []  # List of files that need reading
-        self._reading_files = []  # List of logs currently being read
-        self._reader_requests = {}
+
+        self._reader_requests = {} # Hash of readers who are being contacted
+                                   # to see if they are availabel to read.
+                                   # the key is a message key.
+
+        self._reading_files = {}  # Hash of files that are being read.
+                                  #   the key is the agent name who is reading
 
         self._dir_key = None
+        self._timer = None
+
+    def setNeedsReader(self):
+        if self._timer is None:
+            self._timer = CheckForReaderTimer(self)
+            self.getAgent().addTimer(self._timer)
+
+    def setNoNeedsReader(self):
+        "Stop timer if it exists"
+        if self._timer is not None:
+            self._timer.stop()
+            self._timer = None
 
     def notify(self, evt):
+        # Process to find a reader:
+
+        #  Step 1 - We recieve a LogNeedsReadEvent
+        #   this indicates a log file needs to be read. We start up the
+        #   poll for availabale readers process by generating a 
+        #   CheckForReadersEvent. 
+
+        #  Step 2 - We handle a CheckForReadersEvent
+        #   This event starts the sequence of events necessary to find an
+        #   available LogReader agent. We will need to get a list of all the
+        #   agents in the network (by asking the director). So we send
+        #   a StatusRequest to the director. Also we set a timer to check
+        #   again for readers.
+        
+        #  Step 3 - Handle response from Director
+        #   The message key for our status request to the director is stored
+        #   in self._dir_key. We match to get our response and then go through
+        #   our list of agents to find any LogReader agents. We will send
+        #   StatusRequest messages to each of the LogReader agents, and store
+        #   the key in a hash self._reader_requests
+
+        #  Step 4 - Handle response from LogReader agent
+        #   LogReader StatusResponse key should be stored in 
+        #   self._read_requests hash. If the response has a state that is not
+        #   ReadingState, we will pop off a file that needs reading and send
+        #   a ReadLogRequest to the LogReader. We will record who is reading
+        #   the file in a hash keyed on LogReader name.
+
+        #  Step 5 - Handle response from ReadLogRequest
+        #   We will receive either a OkResponse or a DeniedResponse. If Denied,
+        #   we need to put the file back on the needs_reading list and make
+        #   sure the timer is still going. If OkResponse, we dont' have to
+        #   do anything.
+
+        #  Step 6 - Handle a ReadLogCompleteMessage from the LogReader agent.
+        #   Match the agent up to teh file it was supposed to be reading.
+        #   Remove the log file from the list of logs we are reading.
+        #   Generate a LogReadCompleteEvent to inform the monitor we are done.
+
         if isinstance(evt, find_log_job.LogNeedsReadEvent):
+            #  Step 1 - We recieve a LogNeedsReadEvent
             if len(self._needs_reading) == 0:
                 self.getAgent().addEvent(CheckForReadersEvent(self))
             self._needs_reading.append(evt.getLogPath())
         elif isinstance(evt, CheckForReadersEvent):
+            #  Step 2 - We handle a CheckForReadersEvent
             log.debug("Checking for readers")
             msg = simple.StatusRequest()
 
             assert self._dir_key == None
             self._dir_key = msg.getKey()
 
+            # We may have a latent CheckForReaderTimer events even after all
+            # the logs have been handled. We will just do nothing in that case
             if len(self._needs_reading) > 0:
-                self.getAgent().addTimer(CheckForReaderTimer(self))
+                self.setNeedsReader()
 
-            conn = self.getAgent().getConnection("Director")
-            if conn is None:
-                log.error("Connection to Director not found")
-                return
-            self.getAgent().addEvent(MessageSendEvent(self, msg, conn))
+                conn = self.getAgent().getConnection("Director")
+                if conn is None:
+                    log.error("Connection to Director not found")
+                    return
+                self.getAgent().addEvent(MessageSendEvent(self, msg, conn))
+            else:
+                self.setNoNeedsReader()
+
         elif isinstance(evt, MessageReceivedEvent) and \
              isinstance(evt.getMessage(), message.Response) and \
              self._dir_key == evt.getMessage().getRequestKey():
+            #  Step 3 - Handle response from Director
             log.debug("Recieved response from director")
             self._dir_key = None
             readers = []
@@ -66,9 +131,11 @@ class FindReaderJob(job.Job):
                 msg = simple.StatusRequest()
                 self._reader_requests[msg.getKey()] = r
                 self.getAgent().addEvent(MessageSendEvent(self, msg, r))
+
         elif isinstance(evt, MessageReceivedEvent) and \
              isinstance(evt.getMessage(), message.Response) and \
             self._reader_requests.has_key(evt.getMessage().getRequestKey()):
+            #  Step 4 - Handle response from LogReader agent
                 reader = self._reader_requests[evt.getMessage().getRequestKey()]
                 del self._reader_requests[evt.getMessage().getRequestKey()]
 
@@ -79,4 +146,43 @@ class FindReaderJob(job.Job):
                 else:
                     log.debug("Reader %s is free for reading a logfile" 
                                % reader.getName())
-                    # TODO: Do something about it
+                    req = LogReader.agent.ReadLogRequest()
+                    req.setLogPath(self._needs_reading.pop())
+                    evt = MessageSendEvent(req, self, evt.getSource())
+                    self.getAgent().addEvent(evt)
+
+        elif isinstance(evt, MessageReceivedEvent) and \
+             isinstance(evt.getMessage(), message.Response) and \
+             self._reading_files.has_key(
+                            evt.getSource().getAgentInfo().getName()):
+            #  Step 5 - Handle response from ReadLogRequest
+            reader_name = evt.getSource().getAgentInfo().getName()
+            if isinstance(evt.getMessage(), OkResponse):
+                # We are good to go
+                log.debug("Reader %s is responded ok for reading %s" % 
+                            (reader_name, self._reading_files[reader_name]))
+            elif isinstance(evt.getMessage(), DeniedResponse):
+                log.error("Reading %s denied request to read %s" % 
+                          (reader_name, self._reading_files[reader_name]))
+                file_name = self._reading_files[reader_name]
+                del self._reading_files[reader_name]
+                self._needs_reading.append(file_name)
+                self.setNeedsReader()
+            else:
+                raise Exception("Unknown response for reader")
+
+        elif isinstance(evt, MessageReceivedEvent) and \
+             isinstance(evt.getMessage(), 
+                        LogReader.agent.ReadLogCompleteMessage):
+            #  Step 6 - Handle a ReadLogCompleteMessage from the 
+            #           LogReader agent.
+            reader_name = evt.getSource().getAgentInfo().getName()
+            reader_file = self._reading_files[reader_name]
+            log.debug("Reader %s finished reading %s" % 
+                      (reader_name, reader_file))
+
+            del self._reading_files[reader_name]
+
+            evt = find_log_job.LogReadCompleteEvent(self, reader_file)
+            self.getAgent().addEvent(evt)
+
